f9ef546ed2ada63778d465743aa59dab620fe31d
Andreas Rath <andreas.rath@ondewo.com>
2026-03-12 07:55:31 +0100

    style: fixed code style

--- ondewo_nlu_webhook_server/server/server.py
+++ ondewo_nlu_webhook_server/server/server.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 This is a template script for
     1) setting up a local server for webhook calls
@@ -140,12 +139,12 @@ async def call_case(
         request_json_loaded: dict = json.loads(request_json)
         try:
             webhook_request = WebhookRequest(**request_json_loaded)
-        except (ValidationError, TypeError):
+        except ValidationError, TypeError:
             raise HTTPException(status_code=400, detail="Invalid request format")
     else:
         try:
             webhook_request = WebhookRequest(**request_json)
-        except (ValidationError, TypeError):
+        except ValidationError, TypeError:
             raise HTTPException(status_code=400, detail="Invalid request format")
 
     webhook_request.headers = dict(request.headers)
